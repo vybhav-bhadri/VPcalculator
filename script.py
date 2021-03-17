485d28368009cc66be96ffbd7d1c2a4643a87cf9
Vybhav Bhadri <svbhadri1110@gmail.com>
2021-03-18 03:51:00 +0530

    resolved calculation errors in target_value func

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ def invest(target_value,value_today):
 
 def sumofGP(a, n, r):
     total = (a * (1 - math.pow(r, n ))) / (1- r)
+    total=math.ceil(total)
     target_value = total-a
     return target_value
 
@@ -49,27 +50,28 @@ print("""
 
 
  """)
-mi = int(input("Enter monthly amount of your investment: "))
-no_install = int(input("Enter the total number of installments: "))
-exp_ret = int(input("Enter the expected growth rate for a year: "))
-value_today=int(input("Enter current value of your investment: "))
+mi = int(input("Enter monthly installment amount: "))
+no_install = int(input("Enter number of months installed: "))
+exp_ret = int(input("Enter the expected growth rate per year in %: "))
+value_today=int(input("Enter current portfolio value of your investment: "))
 
 exp_ret=exp_ret/12
 
 target_value = sumofGP(mi, no_install+1, 1+exp_ret/100)
-target_value=math.ceil(target_value)
-invest_amount =invest(target_value,value_today)
-invest_amount = math.ceil(invest_amount)
+target_value=math.floor(target_value)
+invest_amount = invest(target_value,value_today)
+invest_amount = math.floor(invest_amount)
 
 
-def print_value(target_value,value_today,invest_amount):
+def print_value(target_value,value_today,invest_amount,mi):
+    difference=value_today-target_value
     if target_value==value_today:
-        print("Portfolio value is same as target value so invest ",invest_amount)
+        print("Portfolio value is same as target value so invest",invest_amount)
     elif target_value > value_today:
-        print("Portfolio value is less than target value ",target_value," so invest",invest_amount)
+        print("Portfolio value is less than target value",target_value,"so invest",invest_amount,"which is",mi,"-",difference)
     elif target_value < value_today:
-        print("Portfolio value is more than target value ",target_value," so invest",invest_amount)
+        print("Portfolio value is more than target value",target_value,"so invest",invest_amount,"which is",mi,"-",difference)
 
-print_value(target_value,value_today,invest_amount)
+print_value(target_value,value_today,invest_amount,mi)
 
 
